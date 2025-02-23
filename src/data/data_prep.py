0143e79e663c8d7777cce93d4bd107a1cb15bf17
Nik-Pro <nitish7290951173@gmail.com>
2025-02-23 19:22:21 +0530

    Add pipeline with mean

--- src/data/data_prep.py
+++ src/data/data_prep.py
@@ -13,7 +13,7 @@ def load_data(filepath : str)-> pd.DataFrame:
 # test_data = pd.read_csv("./data/raw/test.csv")
 
 
-def fill_missing_with_median(df):
+def fill_missing_with_mean(df):
     try:
         for column in df.columns:
             if df[column].isnull().any():
@@ -29,8 +29,8 @@ def save_data(df : pd.DataFrame, filepath : str)-> None:
     except Exception as e:
        raise Exception(f"Error saving data to {filepath} : {e}")
 
-# train_processed_data= fill_missing_with_median(train_data)
-# test_processed_data=fill_missing_with_median(test_data)
+# train_processed_data= fill_missing_with_mean(train_data)
+# test_processed_data=fill_missing_with_mean(test_data)
 
 def main():
     try:
@@ -40,15 +40,15 @@ def main():
         train_data =  load_data(os.path.join(raw_data_path,"train.csv"))
         test_data = load_data(os.path.join(raw_data_path,"test.csv"))
         
-        train_processed_data = fill_missing_with_median(train_data)
-        test_processed_data = fill_missing_with_median(test_data)
+        train_processed_data = fill_missing_with_mean(train_data)
+        test_processed_data = fill_missing_with_mean(test_data)
 
 # data_path = os.path.join("data","processed")
 
         os.makedirs(processed_data_path)
 
-        save_data(train_processed_data,os.path.join(processed_data_path,"train_processed.csv"))
-        save_data(test_processed_data,os.path.join(processed_data_path,"test_processed.csv"))
+        save_data(train_processed_data,os.path.join(processed_data_path,"train_processed_mean.csv"))
+        save_data(test_processed_data,os.path.join(processed_data_path,"test_processed_mean.csv"))
     except Exception as e:
         raise Exception(f"An error ocurred :{e}")
 
--- src/model/model_building.py
+++ src/model/model_building.py
@@ -62,7 +62,7 @@ def save_model(model : RandomForestClassifier, filepath : str)-> None:
 def main():
     try:
         params_path ="params.yaml"
-        data_path = "./data/processed/train_processed.csv"
+        data_path = "./data/processed/train_processed_mean.csv"
         model_name = "models/model2.pkl"
 
         n_estimators = load_params (params_path)
